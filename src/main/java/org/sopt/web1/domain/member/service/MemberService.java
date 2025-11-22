5c028762b152a06e36e89c27a9bf8f3b833384da
pbem22 <dlacofbs1229@gmail.com>
2025-11-23 06:37:02 +0900

    fix: #9 유저 프로필 사진

--- src/main/java/org/sopt/web1/domain/member/service/MemberService.java
+++ src/main/java/org/sopt/web1/domain/member/service/MemberService.java
@@ -20,6 +20,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private static final String MEMBER_PROFILE = "https://37-sopkathon-web1.s3.ap-northeast-2.amazonaws.com/profile.svg";
 
     public Member getMember(Long memberId) {
         return memberRepository.findById(memberId)
@@ -44,7 +45,7 @@ public class MemberService {
 
         // 2. 해당 닉네임을 가진 회원이 존재하지 않을 때
         String encodedPassword = passwordEncoder.encode(request.password());
-        Member newMember = Member.create(request.nickname(), encodedPassword);
+        Member newMember = Member.create(request.nickname(), encodedPassword, MEMBER_PROFILE);
         memberRepository.save(newMember);
 
         return MemberLoginResponse.of(newMember);
