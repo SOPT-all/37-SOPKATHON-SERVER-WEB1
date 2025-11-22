68aaf6f6c60dcaa7af12d099b2ec55f6b528de77
pbem22 <dlacofbs1229@gmail.com>
2025-11-23 06:55:55 +0900

    fix: controller name 수정

--- src/main/java/org/sopt/web1/domain/video/controller/VideoController.java
+++ src/main/java/org/sopt/web1/domain/video/controller/VideoController.java
@@ -76,7 +76,7 @@ public class VideoController {
         );
     }
 
-    @DeleteMapping("/videos/{videoId}")
+    @DeleteMapping("/{videoId}")
     public ResponseEntity<ApiResponse<Void>> deleteVideo(
             @RequestHeader(name = "memberId") Long memberId,
             @PathVariable(name = "videoId") Long videoId
@@ -86,14 +86,14 @@ public class VideoController {
         return ResponseEntity.ok(ApiResponse.ok(null,"영상이 삭제되었습니다."));
     }
 
-    @GetMapping("/videos/{videoId}")
+    @GetMapping("/{videoId}")
     public ResponseEntity<ApiResponse<VideoResponse>> getVideo(@PathVariable(name = "videoId") Long videoId){
 
         VideoResponse video = videoService.getVideo(videoId);
         return ResponseEntity.ok(ApiResponse.ok(video, "영상을 조회했습니다."));
     }
 
-    @PostMapping("/videos/{videoId}/like")
+    @PostMapping("/{videoId}/like")
     public ResponseEntity<ApiResponse<Void>> likeVideo(
             @RequestHeader(name = "memberId") Long memberId,
             @PathVariable(name = "videoId") Long videoId
