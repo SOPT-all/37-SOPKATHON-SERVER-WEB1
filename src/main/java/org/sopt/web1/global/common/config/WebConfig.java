df0ff0f347d8666f6ed76230b53c9331edeb1c6c
pbem22 <dlacofbs1229@gmail.com>
2025-11-23 05:32:58 +0900

    test

--- src/main/java/org/sopt/web1/global/common/config/WebConfig.java
+++ src/main/java/org/sopt/web1/global/common/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173", "http://localhost:5174") // 모든 출처 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 모든 메소드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(false)
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
