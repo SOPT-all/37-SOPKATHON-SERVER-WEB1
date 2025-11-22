1634af909ead419c5058a0d91bc6c42ad28c52b1
hyomee2 <s5763305@naver.com>
2025-11-23 05:30:58 +0900

    feat: 마이페이지 API 구현

--- src/main/java/org/sopt/web1/domain/like/repository/LikeRepository.java
+++ src/main/java/org/sopt/web1/domain/like/repository/LikeRepository.java
@@ -1,12 +1,22 @@
 package org.sopt.web1.domain.like.repository;
 
 import org.sopt.web1.domain.like.entity.Like;
+import org.sopt.web1.domain.member.entity.Member;
 import org.sopt.web1.domain.video.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
     int countLikeByVideo(Video video);
+
+    @Query("select distinct l.video from Like l where l.member = :member")
+    List<Video> findLikedVideos(@Param("member") Member member);
+
+    Integer countByVideo(Video v);
 }
--- src/main/java/org/sopt/web1/domain/video/repository/VideoRepository.java
+++ src/main/java/org/sopt/web1/domain/video/repository/VideoRepository.java
@@ -1,9 +1,17 @@
 package org.sopt.web1.domain.video.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.sopt.web1.domain.like.entity.Like;
+import org.sopt.web1.domain.member.entity.Member;
 import org.sopt.web1.domain.video.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
+
+    List<Video> findByMember(Member member);
 }
