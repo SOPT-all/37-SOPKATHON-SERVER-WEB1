0fe06effbe28bc016b31f678624e6aa036c369e9
pbem22 <dlacofbs1229@gmail.com>
2025-11-25 23:43:44 +0900

    fix: cors 설정 제거

--- src/main/java/org/sopt/web1/global/common/config/SwaggerConfig.java
+++ src/main/java/org/sopt/web1/global/common/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package org.sopt.web1.global.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,9 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("SOPT Web1 API 명세서")
                         .description("SOPT 37기 Web1팀 API 명세서입니다.")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Local"))
+                .addServersItem(new Server().url("https://nvp.kr").description("Production"));
+
     }
 }
